dc39faff9673273f3c8062a88bd9cedba84c21c1
MarineGift <marinegift4u@gmail.com>
2025-07-30 03:57:56 -0400

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,11 @@
 import { getRequestConfig } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 
+
 // 지원하는 언어 목록
 export const locales = ['en', 'ko']
 export type Locale = 'en' | 'ko'
+export const defaultLocale = 'en'; 
 
 export default getRequestConfig(async ({ locale }) => {
   // 지원하지 않는 언어인 경우 404 처리

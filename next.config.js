e2a296fc9127f77293aca0c9c5c2a6340391b474
MarineGift <marinegift4u@gmail.com>
2025-07-30 00:14:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
-cat > next.config.js << 'EOF'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // TypeScript 에러를 완전히 무시
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint 에러를 완전히 무시
     ignoreDuringBuilds: true,
   },
-  // Webpack에서 TypeScript 체크 플러그인 제거
   webpack: (config, { dev, isServer }) => {
-    // TypeScript 체크 플러그인들을 필터링해서 제거
     config.plugins = config.plugins.filter((plugin) => {
       return ![
         'ForkTsCheckerWebpackPlugin',
@@ -26,4 +21,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-EOF

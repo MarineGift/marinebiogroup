c7ad8380eb37b9a90e2f24d76a777ddbc8365439
MarineGift <marinegift4u@gmail.com>
2025-08-04 14:55:46 -0400

    z

--- src/i18n/config.ts
+++
@@ -1,6 +0,0 @@
-// src/i18n/config.ts
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../locales/${locale}.json`)).default,
-}));
--- src/i18n/request.ts
+++
@@ -1,9 +0,0 @@
-// i18n/request.ts
-import { getRequestConfig } from 'next-intl/server';
-
-export const locales = ['en', 'ko'] as const;
-export const defaultLocale = 'ko' as const;
-
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../messages/${locale}.json`)).default
-}));

646bd41abdd44f50d0066c8ea5c94203bb86c28e
MarineGift <marinegift4u@gmail.com>
2025-08-05 03:24:03 -0400

    a

--- src/app/service/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ServicePage() {
-  return (
-    <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-2xl font-bold">서비스 안내</h1>
-      <p className="mt-4">제공하는 서비스 내용이 여기에 표시됩니다.</p>
-    </div>
-  );
-}
